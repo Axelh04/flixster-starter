68745f2a8dc113ff871f4412baccb380fde3d5e8
Axelh04 <ahernandez53@hawk.iit.edu>
2024-06-14 21:06:51 -0700

    Final Submission
    Added another stretch feature! Cleaned up code.

--- src/MovieCard.jsx
+++ src/MovieCard.jsx
@@ -1,10 +1,22 @@
 import "./MovieCard.css";
+import { useState } from "react";
 
 /* eslint-disable react/prop-types */
 
 function MovieCard({ title, poster, votes, id, onSelectMovie }) {
+  const [like, setLike] = useState("♡");
+  const [watched, setWatched] = useState("▫️");
+
   const handleChange = () => {
     onSelectMovie(id);
+  };
+
+  const handleLike = () => {
+    setLike("♥");
+  };
+
+  const handleWatch = () => {
+    setWatched("☑");
   };
 
   return (
@@ -15,6 +27,12 @@ function MovieCard({ title, poster, votes, id, onSelectMovie }) {
         src={`https://image.tmdb.org/t/p/w500${poster}`}
       />
       <h2 id="title">{title}</h2>
+      <h2 id="like" onClick={handleLike}>
+        {like}
+      </h2>
+      <h2 id="watched" onClick={handleWatch}>
+        {watched}
+      </h2>
       <h1 id="votes">{votes}</h1>
     </div>
   );
